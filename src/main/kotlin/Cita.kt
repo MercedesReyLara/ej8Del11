1036b7bbdc3f4e176351655108ca1624801970eb
Mercedes <merel2060@gmail.com>
2022-11-09 10:01:59 +0100

    HOLA

--- src/main/kotlin/Cita.kt
+++ src/main/kotlin/Cita.kt
@@ -1,2 +1,7 @@
-class Cita(var numCita:Int, var horaCita:Int, var diaCita:Int) {
+class Cita(var numCita:Int, var horaCita:Int, var diaCita:Int,var acudio:Boolean,var llamada:Boolean) {
+    fun eliminar(){
+        if(llamada){
+            cit.remove(this)
+        }
+    }
 }
--- src/main/kotlin/Funciones.kt
+++ src/main/kotlin/Funciones.kt
@@ -4,7 +4,7 @@ fun pedirCita() {
         println("***BIENVENIDO AL MENÚ DE CITAS***")
         println("OPCIONES: ")
         println("1)Pedir cita")
-        println("2)Ver calendario")
+        println("3)Ver y gestionar calendario de citas")
         print("ESCOJA UNA OPCION: ")
         var tmp1:Int?=readLine()?.toIntOrNull()
         var op:Int=tmp1?:0
@@ -14,50 +14,82 @@ fun pedirCita() {
             var dia: Int = tmp ?: 1
             if (dia > 31 || dia < 1) {
                 println("DIA NO DISPONIBLE.VOLVIENDO AL MENU")
+                break
             } else {
                 println("***ABRIMOS A LAS 9 Y CERRAMOS A LAS 21***")
-                    print("ESCOJA HORA DE LA CITA: ")
-                    var tmp2: Int? = readLine()?.toIntOrNull()
-                    var hora: Int = tmp2 ?: 1
+                print("ESCOJA HORA DE LA CITA: ")
+                var tmp2: Int? = readLine()?.toIntOrNull()
+                var hora: Int = tmp2 ?: 1
                 if (hora < 9 || hora >= 21) {
                     println("HORA NO DISPONIBLE, NO ESTAMOS ABIERTOS.VOLVIENDO AL MENU")
-                } else
-                            print("Esta es su cita: dia $dia, hora $hora")
-                            print("¿Desea confirmar la cita(S/N)?")
-                            var a:String?=readLine()
-                            var r:String=a.toString()
-                            if(a=="S"){
-                                var num: Int = (Math.random()*10000+1).toInt()
-                                for(cits in cit){
-                                    if(dia==cits.diaCita && hora==cits.horaCita){
-                                        println("Ya hay una cita existente con ese dia y hora")
-                                        /*print("¿Desea cambiar la hora(S/N)? ")
-                                        var c:String?=readLine()
-                                        var ca:String=c.toString()
-                                        if(ca=="S"){
-                                            println("***ABRIMOS A LAS 9 Y CERRAMOS A LAS 21***")
-                                            print("ESCOJA HORA DE LA CITA: ")
-                                            var tmp2: Int? = readLine()?.toIntOrNull()
-                                            var hora: Int = tmp2 ?: 1
-                                            if (hora < 9 || hora >= 21) {
-                                                println("HORA NO DISPONIBLE, NO ESTAMOS ABIERTOS.VOLVIENDO AL MENU")
-                                        }
-                                    }*/
-                                    }else{
-                                        var cita = Cita(num, dia, hora)
-                                        cit.add(cita)
-                                        break
-                                }
-
+                    break
+                } else {
+                    println("Esta es su cita: dia $dia, hora $hora")
+                    print("¿Desea confirmar la cita(S/N)?")
+                    var a: String? = readLine()
+                    var r: String = a.toString()
+                    if (a == "S") {
+                        for (cits in cit) {
+                            if (dia == cits.diaCita && hora == cits.horaCita) {
+                                println("Ya hay una cita existente con ese dia y hora")
+                                break
+                            }
+                        }
+                            print("CITA ENVIADA A LA OFICINA")
+                        var num: Int = (Math.random() * 10000 + 1).toInt()
+                            var cita = Cita(num, hora, dia,true,false)
+                            println("DATOS DE LA CITA: La cita es el día ${cita.diaCita} a las ${cita.horaCita}")
+                            print("¿Hola administrador, desea confirmar esta cita(S/N)?")
+                            var r: String? = readLine()
+                            var a: String = r.toString()
+                            if (a == "S") {
+                                cit.add(cita)
+                            } else {
+                                println("Cita denegada")
+                                break
                             }
 
+                        }else{
+                            println("VOLVIENDO AL MENÚ")
+                    }
                     }
                 }
         }else if(op==2){
-                    for (cits in cit) {
-                        println("CITA NUM:${cits.numCita}")
-                        println("Dia de la cita:${cits.diaCita};Hora de la cita:${cits.horaCita}")
-                    }
+            if(cit.size==0) {
+                println("No hay citas en la lista")
+            }else{
+                for (cits in cit) {
+                    println("CITA NUM:${cits.numCita}")
+                    println("Dia de la cita:${cits.diaCita};Hora de la cita:${cits.horaCita}")
+                }
+
+            }
+        }else if(op==3){
+            if(cit.size==0){
+                println("No hay citas")
+            }else {
+                println("ESTE ES EL CALENDARIO")
+                for ((cits,cia) in cit.withIndex()) {
+                    var i:Int=cits
+                    println("CITA NUM:${cia.numCita}")
+                    println("Dia de la cita:${cia.diaCita};Hora de la cita:${cia.horaCita}")
+                        println("¿Acudió a la cita?")
+                        var x: String? = readLine()
+                        var a: String = x.toString()
+                        if (a == "S") {
+                            println("Eliminando de la lista")
+
+                        } else {
+                            if (!cia.llamada) {
+                            println("Haciendo una llamada para confirmar que va a venir")
+                            cia.llamada = true
+                            }else{
+                                println("No respondió a la llamada, lo eliminaremos de la lista")
+                                cia.eliminar()
+                            }
+                        }
+                }
+            }
         }else{
             println("OPCIÓN NO DISPONIBLE")
         }
